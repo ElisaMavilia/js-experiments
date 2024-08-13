3ae595a3ef8a3ea58802ebd5f75bb11bfba365fd
ElisaMavilia <elisamavilia1@gmail.com>
2024-08-13 07:33:20 +0200

    adjustments

--- experiments.js
+++ experiments.js
@@ -4,21 +4,48 @@
 
 function saveNumsAndConvert(){
 
-    const num =  Math.floor((Math.random()*3)+1);
+    let num =  Math.floor((Math.random()*3)+1);
 
     if (num === 1){
-        console.log('scissors');
+        num = 'scissors'
+        console.log(num);
     } else if(num === 2){
-        console.log('rock');
+        num = 'rock'
+        console.log(num);
     } else if(num === 3){
-        console.log('paper');
+        num = 'paper'
+        console.log(num);
     }
 
     return num;
 }
 
-const conversion = saveNumsAndConvert();
+/* const conversion = saveNumsAndConvert(); */
 
+
+function userMatch(){
+
+    const userOne = saveNumsAndConvert();
+    const userTwo = saveNumsAndConvert();
+
+   if(userOne === "scissors" && userTwo === "rock"){
+    console.log(`User 2 wins! Rock wins over the Scissors`);
+   } else if (userOne === "scissors" && userTwo=== "paper"){
+    console.log(`User 1 wins! Scissors wins over the Paper`);
+   } else if (userOne === "rock" && userTwo === "paper"){
+    console.log(`User 2 wins! Paper wins over the rock`);
+    } else if (userOne === "rock" && userTwo === "scissors"){
+        console.log(`User 1 wins! Rock wins over the Scissors`);
+    } else if(userOne === "paper" && userTwo === "scissors"){
+        console.log(`User 2 wins! Scissors wins over the Paper`);
+    } else if (userOne === "paper" && userTwo === "rock"){
+        console.log(`User 1 wins! Paper wins over the rock`);
+    } else if (userOne === userTwo){
+        console.log('Pareggio! Try again...');
+    }
+}
+
+const result = userMatch();
 
 
 
